3a64a1f20c463f7c01839ca5fa9deb972684482d
mikemac18 <53865195+mikemac18@users.noreply.github.com>
2021-01-30 23:18:32 -0500

    Update managerbot_test.py

--- managerbot_test.py
+++ managerbot_test.py
@@ -2,15 +2,29 @@ from slack import WebClient
 from managerbot import ManagerBot
 import os
 import xlrd
+import time
+from datetime import datetime, timedelta
 
-# Create a slack client
+# Create a timestamp for tomorrow at 9AM
+#today = datetime.date.today()
+#scheduled_time = datetime.time(hour=19, minute=50)
+#schedule_timestamp = datetime.datetime.combine(today, scheduled_time).strftime('%s')
+
+
+
 slack_web_client = WebClient(token=os.environ.get("BOT_USER_TOKEN"))
-print(os.environ.get("BOT_USER_TOKEN"))
-# Get a new CoinBot
-manager_bot = ManagerBot("#general")
 
-# Get the onboarding message payload
-message = manager_bot.get_message_payload()
+while True:
+#def funcSend():
+    # Create a slack client
 
-# Post the onboarding message in Slack
-slack_web_client.chat_postMessage(**message)
+
+    # Get a new CoinBot
+    manager_bot = ManagerBot("#general")
+
+    # Get the onboarding message payload
+    message = manager_bot.get_automated_early_message_payload()
+
+    # Post the onboarding message in Slack
+    slack_web_client.chat_postMessage(**message)
+    time.sleep(86400)
