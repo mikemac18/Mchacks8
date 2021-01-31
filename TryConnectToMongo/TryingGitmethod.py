7e78722b8db84b383d563f08ce5e0f18a0eac477
mikemac18 <53865195+mikemac18@users.noreply.github.com>
2021-01-31 05:47:59 -0500

    Delete TryingGitmethod.py

--- TryConnectToMongo/TryingGitmethod.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-uri = "mongodb+srv://cluster0.jweue.mongodb.net/<dbname>?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
-client = MongoClient(uri,
-                     tls=True,
-                     tlsCertificateKeyFile='<path_to_certificate>')
-db = client['testDB']
-collection = db['testCol']
-doc_count = collection.count_documents({})
-print(doc_count)
