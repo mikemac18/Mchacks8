e4ac0b496f25bd192bb18061c9aea047b60804a9
mikemac18 <53865195+mikemac18@users.noreply.github.com>
2021-01-31 05:47:47 -0500

    Delete Test4.py

--- TryConnectToMongo/Test4.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://cluster0.jweue.mongodb.net")
-db = client.test
-col = db.tasklist
-result = col.find( {"some field": "Michael"} )
-for doc in result:
-    # print the document's _id to terminal
-    print ("doc _id:", doc["_id"])
